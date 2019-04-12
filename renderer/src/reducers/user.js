181f639ce523d0f8a9add788341c3df97d11f4f0
Zeroslope <738543046@qq.com>
2019-04-12 19:18:59 +0800

    delete initialState

--- renderer/src/reducers/user.js
+++ renderer/src/reducers/user.js
@@ -1,35 +1,6 @@
 import { ADD_USER_PROJECT, DEL_USER_PROJECT, DEL_ALL_USER_PROJECT } from '../types'
 
-const initialState = [
-  {
-    index: 0,
-    isNeedImage: false,
-    pageNumber: 4,
-    userId: '1729332983',
-    username: '复旦大学'
-  },
-  {
-    index: 1,
-    isNeedImage: false,
-    pageNumber: 4,
-    userId: '1729332983',
-    username: '复旦大学'
-  },
-  {
-    index: 2,
-    isNeedImage: false,
-    pageNumber: 4,
-    userId: '1729332983',
-    username: '复旦大学'
-  },
-  {
-    index: 3,
-    isNeedImage: false,
-    pageNumber: 4,
-    userId: '1729332983',
-    username: '复旦大学'
-  }
-]
+const initialState = []
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
